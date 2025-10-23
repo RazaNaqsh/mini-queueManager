60a38ca46883f2ff30cb654593a1cdbc81d4aabc
RazaNaqsh <owaisraza1704@gmail.com>
2025-10-23 23:40:01 +0530

    Add retry mechanism

--- app/tasks.py
+++ app/tasks.py
@@ -1,9 +1,15 @@
 from .celery_app import celery_app
-import time
+import time, random
 
-@celery_app.task(bind=True)
+@celery_app.task(bind=True, max_retries=3, default_retry_delay=5)
 def process_data_task(self, text: str):
-    """Simulate a time-consuming task"""
-    print(f"Worker received: {text}")
-    time.sleep(10)  # simulate work
-    return {"original": text, "upper": text.upper(), "length": len(text)}
+    """Simulate a time-consuming and error-prone task"""
+    try:
+        print(f"Worker received: {text}")
+        time.sleep(2)
+        # Simulate random failure
+        if random.random() < 0.7:
+            raise ValueError("Random failure occurred!")
+        return {"original": text, "upper": text.upper(), "length": len(text)}
+    except Exception as exc:
+        raise self.retry(exc=exc)
